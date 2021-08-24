ef505c57e1a9da6ec844fef95a2aad7453ffbac7
guomm2019-star <56241632+guomm2019-star@users.noreply.github.com>
2021-08-24 20:24:08 +0800

    Update CoDOSA-PLS.py

--- CoDOSA-PLS.py
+++ CoDOSA-PLS.py
@@ -2,3 +2,11 @@
 # -*- coding:utf-8 -*-
 #author: MingmingGuo
 #If you use CoDOSA model in your research, please cite the following paper:M. M. Guo, W. Cui*, Theoretical investigation of metal nonmetal co decorated graphyne 1 for electrocatalytic water splitting by DFT stud y and CoD O SA model. Submitted, 2021.
+
+
+
+
+
+
+
+
